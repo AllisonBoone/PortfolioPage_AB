7683daf85762886e62b0afe7d2e195834847ca6d
Allison <Allison.Boone@keyin.com>
2024-10-14 19:16:48 -0230

    Initial commit

---
+++ New Portfolio Project/Weather Dashboard/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/weather', async (req, res) => {
+  const city = req.query.city || "St. John's";
+  try {
+    const response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY`
+    );
+    const weatherDate = response.data;
+    res.render('weather', { weather: weatherData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error retrieving weather data');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
